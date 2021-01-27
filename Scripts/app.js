eb496bce3d2f37c91613b0ad142b9674409fa9a0
Scott Alton <scottaltonmusic@gmail.com>
2021-01-27 10:01:14 -0500

    Contact class into separate file.

--- Scripts/app.js
+++ Scripts/app.js
@@ -6,45 +6,6 @@
 
 "use strict";
 
-// Contact class
-class Contact {
-  // constructor
-  constructor(fullName, contactNumber, email) {
-    this.m_fullName = fullName;
-    this.m_contactNumber = contactNumber;
-    this.m_email = email;
-  }
-  // methods
-  toString() {
-    return `Full Name:         ${this.m_fullName}\nContact Number:    ${this.m_contactNumber}\nEmail Address:     ${this.m_email}`;
-  }
-
-  // Accessor and mutators
-  get FullName() 
-  {
-    return this.m_fullName;
-  }
-  set FullName(value) 
-  {
-    this.m_fullName = value;
-  }
-  get ContactNumber() 
-  {
-    return this.m_contactNumber;
-  }
-  set ContactNumber(value) {
-    this.m_contactNumber = value;
-  }
-  get Email() 
-  {
-    return this.m_email;
-  }
-  set Email(value) 
-  {
-    this.m_email = value;
-  }
-}
-
 (function () 
 {
   function displayHome() 
